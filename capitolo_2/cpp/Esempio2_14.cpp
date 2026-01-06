d6dd8b62e211848e236efbab2c68de6459c1fa1f
Simone Acerbi <simone.acerbi1995@gmail.com>
2026-01-06 11:52:48 +0100

    Fix Esempio 2.14

--- capitolo_2/cpp/Esempio2_14.cpp
+++ capitolo_2/cpp/Esempio2_14.cpp
@@ -7,55 +7,38 @@
 #include "myDiffEquation.h"
 
 namespace plt = matplotlibcpp;
-
-// --------------------------
-// Parametri di sistema
-// --------------------------
-struct Params {
-    double J;
-    double k;
-};
-
-double f_sign(double alpha){
-    return alpha != 0 ? alpha/abs(alpha) : 0;
-}
-
-// --------------------------
-// Equazione differenziale - Controllore ad anello chiuso
-// --------------------------
-std::vector<double> f_sistemaCentrifuga(double t, const std::vector<double>& x, const Params& p, double& u) {
-    (void)t; //unuser
-    double dx = -(p.k/p.J)*x[0]*x[0]*f_sign(x[0]) + (1/p.J)*u;
-    return {dx};
-}
-
 // --------------------------
 // MAIN
 // --------------------------
 int main() {
     // Dati
-    double J = 1, k = 0.1, u1 = 0.0, u2 = 0.5, w0 = 1;
-    double dt = 0.01;
-    double t0 = -5, tf = 5;
-    int N = static_cast<int>((tf - t0) / dt);
+    double u1 = 1.0/8.0, u2 = 1.0/4.0, u3 = 1.0/2.0;
+    double dx = 0.01;
+    double x0 = -2, xf = 2;
+    int N = static_cast<int>((xf - x0) / dx);
     std::vector<double> x(N + 1);
-    for (int i = 0; i <= N; ++i) x[i] = t0 + i * dt;
-
-    // Calcolo ybar all'equilibrio per entrambi gli input
-    std::vector<double> w1(N+1);
-    std::vector<double> w2(N+1);
+    for (int i = 0; i <= N; ++i) x[i] = x0 + i * dx;
+    // Calcolo il comportamento dei punti di equilibrio al variare di x
+    std::vector<double> y1(N+1);
+    std::vector<double> y2(N+1);
+    std::vector<double> y3(N+1);
 
     for (int i = 0; i<= N; i++){
-        w1[i] = -(k/J) * x[i] * x[i] * f_sign(x[i]) + u1/J;
-        w2[i] = -(k/J) * x[i] * x[i] * f_sign(x[i]) + u2/J;
+        y1[i] = x[i] * x[i] + x[i] + u1;
+        y2[i] = x[i] * x[i] + x[i] + u2;
+        y3[i] = x[i] * x[i] + x[i] + u3;
     }
     // Plot Figura 2.4
     plt::figure(1);
-    plt::plot(x, w1, {{"label", "w(t), u(t) = 0"}});
-    plt::plot(x, w2, {{"label", "w(t), u(t) = 0.5"}});
-    plt::xlabel("Tempo (t)");
-    plt::ylabel("Velocità angolare");
-    plt::title("Comportamento del sistema centrifuga");
+    plt::plot(x, y1, {{"label", "y(x), ubar = 0.125"}});
+    plt::plot(x, y2, {{"label", "y(x), ubar = 0.25"}});
+    plt::plot(x, y3, {{"label", "y(x), ubar = 0.5"}});
+    plt::plot({-2, 2}, {0, 0}, {{"label", "y = 0"}, {"linestyle", "--"}});
+    plt::xlim(-2, 2);
+    plt::ylim(-1, 2);
+    plt::xlabel("x");
+    plt::ylabel("y");
+    plt::title("Variazione equilibrio al variare di ubar");
     plt::legend();
     plt::grid(true);
     
